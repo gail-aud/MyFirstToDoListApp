cd7b0bb693ceb25d736346dfe3bd360dabf8c945
gail-aud <gail.javier@edu.sait.ca>
2023-11-15 16:00:58 -0700

    Lab 5 🥲

---
+++ src/screens/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function About({ }) {
+    return(
+        <>
+         <View>
+            <Text>Gail's Awesome To Do List App</Text>
+            <Text>Created by: Gail Audrey Javier</Text>
+            <Text>Version 1.1906</Text>
+
+         </View>
+        </>
+    );
+}
+
+export default About;
